4b67259a06b1473b6c520580970b89723b50cccf
zzy <1184271384@qq.com>
2024-07-14 09:04:37 +0800

    本地注册器

--- example-provider/src/main/java/com/yupi/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/yupi/example/provider/EasyProviderExample.java
@@ -1,5 +1,7 @@
 package com.yupi.example.provider;
 
+import com.yupi.example.common.service.UserService;
+import com.yupi.yurpc.registry.LocalRegistry;
 import com.yupi.yurpc.server.HttpServer;
 import com.yupi.yurpc.server.VertxHttpServer;
 
@@ -11,7 +13,12 @@ public class EasyProviderExample
 {
     public static void main( String[] args ) {
         System.out.println( "example-provider 服务启动..." );
+
+        // 注册服务，rpc中的本地服务注册器
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
+        // 启动 Web 服务
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);   // 启动 HTTP 服务
+        httpServer.doStart(8080);
     }
 }
