b590411b84ccb7a854efbc53d23544025a1bd40f
zzy <1184271384@qq.com>
2024-07-15 21:42:45 +0800

    (全局配置加载)主要文件

---
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/constant/RpcConstant.java
@@ -0,0 +1,14 @@
+package com.yupi.yurpc.constant;
+
+/**
+ * RPC 相关常量
+ */
+public class RpcConstant {
+
+    /**
+     * 默认配置文件加载前缀
+     */
+    public static final String DEFAULT_CONFIG_PREFIX = "rpc";
+
+//    String DEFAULT_CONFIG_PREFIX = "rpc";
+}
